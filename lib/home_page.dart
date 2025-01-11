5909112e1b7598e3652248f50e09e6ed3fa80dcc
Shubham01920 <shubhammaurya2442@gmail.com>
2025-01-12 03:36:57 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      drawer: Drawer(),
+      body: Center(child: Text("sfd")),
+    );
+  }
+}
